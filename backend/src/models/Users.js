73cb59146754148cc4869962f796282b47219737
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-15 12:54:46 +0500

    new changes

--- backend/src/models/Users.js
+++ backend/src/models/Users.js
@@ -48,8 +48,6 @@ const userSchema = new mongoose.Schema(
         ref: "User",
       },
     ],
-    
-    
   },
   { timestamps: true }
 );
@@ -57,18 +55,18 @@ const userSchema = new mongoose.Schema(
 const User = mongoose.model("User", userSchema);
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) {
-        return next();
-    }
+  if (!this.isModified("password")) {
+    return next();
+  }
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-        this.password = hashedPassword;
-        next();
-    } catch (error) {
-        next(error);
-    }
-})
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default User;
