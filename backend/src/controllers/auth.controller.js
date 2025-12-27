25ef5fff1940b235df6c6abe245205521d6ae55c
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-27 17:04:22 +0500

    SIgnup + Login + Logout controllers are added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -27,7 +27,7 @@ export async function signup(req, res) {
     const idx = Math.floor(Math.random() * 100) + 1;
     const randomeAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
 
-    const newUser = new User({
+    const newUser = await User.create({
       fullName,
       email,
       password,
@@ -41,13 +41,63 @@ export async function signup(req, res) {
         expiresIn: "7d",
       }
     );
-  } catch (error) {}
+    
+    res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "Strict",
+    secure: process.env.NODE_ENV == "production",
+    });
+    res.status(201).json({success: true, user: newUser});
+    
+  } catch (error) {
+  console.log("Error creating user:", error);
+  res.status(500).json({ message: "Error creating user" });
+  }
 }
 
-export function login(req, res) {
-  res.send("Login Route");
+export async function login(req, res) {
+  try {
+    const {email, password } = req.body;
+    
+    if(!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    
+    const user = await User.findOne({ email });
+    if(!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    
+    const isPasswordCorrect = await user.matchPassword(password);    
+    if(!isPasswordCorrect) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+    
+    res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "Strict",
+    secure: process.env.NODE_ENV == "production",
+    });
+    
+    res.status(200).json({success: true, user});
+    
+  } catch (error) {
+    console.log("Error in Login controller ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
-export function logout(req, res) {
-  res.send("Logout Route");
+export async function logout(req, res) {
+  res.clearCookie("jwt");
+  res.status(200).json({success: true});
 }
